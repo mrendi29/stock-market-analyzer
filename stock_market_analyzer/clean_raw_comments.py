5029a2f93edd3cac96f015a5359847696586ac40
Endi Caushi <42871239+mrendi29@users.noreply.github.com>
2021-07-29 00:17:21 +0200

    Update clean_raw_comments.py

--- stock_market_analyzer/clean_raw_comments.py
+++ stock_market_analyzer/clean_raw_comments.py
@@ -1,18 +1,21 @@
 import pandas as pd
-
-# Only way to extract needed fields from 4gb csv file in order to work collaboratively.
+import csv
+# Only way to extract needed fields from 4gb csv file and then compress
+# in order to work collaboratively.
 with pd.read_csv(
-    "wallstreetbets_comments.csv",
+    "../wallstreetbets_comments.csv",
     usecols=["created_utc", "id", "link_id", "body", "score"],
     chunksize=250000,
 ) as reader:
     for chunk in reader:
+        chunk['body'] = chunk['body'].str.replace(r"\,|\n",r'',regex=True)
         with open("comments.csv", "a", newline="") as f:
-            chunk.to_csv(
-                f,
-                mode="a",
-                sep="|",
-                header=f.tell() == 0,
-                index=False,
-                line_terminator="\r\n",
+                chunk.to_csv(
+                    f,
+                    mode="a",
+                    sep=",",
+                    header=f.tell() == 0,
+                    index=False,
+                    quoting=csv.QUOTE_MINIMAL,
+                    quotechar="|",
             )
